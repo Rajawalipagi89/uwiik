c867c77c6fb0d0bb19cdc10313d8091da0110aab
Arif Hendrawan Priliyanto <64543036+ahprojects023@users.noreply.github.com>
2021-02-01 21:13:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Dwi Febryanti" && b=="02-02-2001")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
